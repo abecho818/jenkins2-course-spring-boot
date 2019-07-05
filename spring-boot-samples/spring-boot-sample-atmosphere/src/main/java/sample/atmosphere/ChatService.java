efaa2b811c3e49a93d99cbb6ec654c36fe825875
Abraham Cho <q1431722@QSD1250796.qvcdev.qvc.net>
2019-07-05 12:26:13 -0400

    dev branch change

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/ChatService.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/ChatService.java
@@ -11,7 +11,7 @@
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONfdvfvS OF ANY KIlkyultguiltguyiltguiylguilguilguilND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License.asdfvsdfvsdfasdfasdfasdf
+ * limitations under the License.asdfvsdfvsdfasdfasdfasdasdvasdvasdvasdvasdvf
  */
 
 package sample.atmosphere;
