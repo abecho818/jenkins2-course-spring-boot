754367add6c3a9a1f29244142ca3b3533bb98b03
Abraham Cho <q1431722@QSD1250796.qvcdev.qvc.net>
2019-07-22 11:02:30 -0400

    dsafasdfasfffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffasdfasdfa

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/Message.java
@@ -9,7 +9,7 @@
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the Licensesgfsdgsdfgsfgsdfgsfdg is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIfdgsfdgsdfgfgsadfasdfasdfadsfdsONS OF ANY KIND, either express or implied.
+ * WITHOUT WARRANTIES OR CONDITIfdgsvfdvffvdssdfvfdgsdfgfgsadfasdfasdfadsfdsONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
