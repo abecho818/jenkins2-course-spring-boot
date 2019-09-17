f5d0c4ee5818e62bddf678f476cb43f0a9f783df
Abraham Cho <q1431722@QSD1250796.qvcdev.qvc.net>
2019-09-17 11:12:37 -0400

    Changes files again.... dalfjvbaldfjbvnkljadfnvlkjadfnvlkjadfnvljkafdnvjkladfnvkljsdfnlvkjsdnfvkljsdfnlkvjnsdfkljvnskdjlfnvskjdfnvlksjdfnvjklsdfnvkljsdfnvkljsdfnkvljsdfnjvklsdfnjvksndfkljvnsdfkjlnvlksjdfnv

--- spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
+++ spring-boot-samples/spring-boot-sample-atmosphere/src/main/java/sample/atmosphere/SampleAtmosphereApplication.java
@@ -16,6 +16,24 @@
 >>>>>>> 35b9cbff642dba9f1694495b731dbdbdfbsdfbbafderbabaregaerg language governing permissions and
  * limitations under the License.
  */
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed undefvzdfvr the Apache License, Version 2.0 (the "License");
+ * you may not use dfgbdfgbdfgbdfgbdfbgdfgbthis file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under theasdcdscdsacsdcsd License is distributed on an "AS IS" BASIS,
+<<<<<<< HEAD
+ * WITHOUT WARRANTIES OR CONDITIONSasasdsadsdasdasdasadsdasda OF ANY KIND, either express or implied.
+=======ddsdsdsdsddsfds
+ * WITHOUT WARRANTIES OR CONDITIONScsdcsdcsdcsdcscd OF ANY KIND, either express or implied.
+>>>>>>> 35b9cbff642dba9f1694495b731dbdbdfbsdfbbafderbabaregaerg language governing permissions and
+ * limitations under the License.
+ */
 
 package sample.atmosphere;
 
